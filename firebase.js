ddf445666b6ea3283a7b9d1ca20e7fb8369b6152
Abdülkadir <141135041+abdulkadiryugruk@users.noreply.github.com>
2024-11-14 12:03:02 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,12 +3,7 @@ import "firebase/compat/auth";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCd0UpccZjSZkrqYcViz7Xjg5x2gmY1uz4",
-  authDomain: "authentication-app-38f7e.firebaseapp.com",
-  projectId: "authentication-app-38f7e",
-  storageBucket: "authentication-app-38f7e.firebasestorage.app",
-  messagingSenderId: "167424994658",
-  appId: "1:167424994658:web:b2439329cb66dca342903b"
+
 };
 
 // Initialize Firebase
